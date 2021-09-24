8a8370ef8d36ac1b5cf46d4d435da6ad58adf7cb
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-09-24 02:04:18 +0200

    Dealing with CMSIS mapping . . .

--- MesDrivers/Sources/MyTimer.c
+++ MesDrivers/Sources/MyTimer.c
@@ -1,5 +1,7 @@
 #include "MyTimer.h"
 
+//Initializes the timer with prescaling and autoreload 
+//T = (1/TIMxCLK) * (PSC+1) * (ARR+1)
 void MyTimer_Base_Init(MyTimer_Struct_TypeDef * TimerParam){
 	TimerParam->Timer->PSC = TimerParam->PSC;
 	TimerParam->Timer->ARR = TimerParam->ARR;	
@@ -14,3 +16,14 @@ void MyTimer_Base_Start(TIM_TypeDef * Timer){
 void MyTimer_Base_Stop(TIM_TypeDef * Timer){
 	Timer->CR1 &= ~(0x01) ;
 }
+
+//Activates the overflow interuption of the timer and sets the priority to Prio
+//0 highest to 15 lowest
+void MyTimer_ActiveIT(TIM_TypeDef * Timer, char Prio){
+	//UIE bit = update interuption. Overflow is an update 
+	Timer->DIER ^=  0x01 ; 
+	
+	//Setting the priority: 0bXXXX0000 
+	NVIC->IPR0
+}
+
