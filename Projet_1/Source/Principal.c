e56340732a496d2d48f02fca2e2ef8a4d38bb8f6
Kyriios188 <robin.siim188@gmail.com>
2021-09-27 12:10:01 +0200

    La PWM, chapitre 155 : il n'y a plus d'espoir, nous mangeons nos morts.

--- Projet_1/Source/Principal.c
+++ Projet_1/Source/Principal.c
@@ -1,5 +1,6 @@
 #include "stm32f10x.h"
 #include "MyTimer.h"
+#include "MyGPIO.h"
 
 int n = 0;
 void handler_timer(void){
@@ -9,16 +10,19 @@ void handler_timer(void){
 
 int main(void)
 {
+	MyGPIO_Struct_TypeDef GPIOx = {GPIOA, 7, AltOut_Ppull};
 	/**INITIALISATION DES VARIABLES**/
-	MyTimer_Struct_TypeDef timer_param = {TIM2, 4499, 7999}; //Structure paramètre de la fonction timer init
+	MyTimer_Struct_TypeDef timer_param = {TIM2, 0, 0xE0F}; //Structure paramètre de la fonction timer init
 	/********************************/
-	
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN;
+	
+	MyGPIO_Init(&GPIOx);
+	MyTimer_PWM(TIM2, 1);
 	
 	MyTimer_Base_Init(&timer_param);
 	MyTimer_Base_Start(TIM2);
 	MyTimer_ActiveIT(TIM2,1,&handler_timer);
-	
 	
 	do {
 		
