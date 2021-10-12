154cfda074b92d1e07741d743b31870caa2ed76b
Kyriios188 <36565218+Kyriios188@users.noreply.github.com>
2021-10-12 13:03:10 +0200

    Update MyGPIO.c
    Ajouté -8 qui manquait dans le cas du CRH

--- MesDrivers/Source/MyGPIO.c
+++ MesDrivers/Source/MyGPIO.c
@@ -1,14 +1,14 @@
 #include "MyGPIO.h"
 #define PIN_NUMBER (5)
 
-//Renvoie la valeur trouvée dans le Control Register correspondant au pin
+//Renvoie la valeur trouvÃ©e dans le Control Register correspondant au pin
 //Je sais pas si c'est ce qui est attendu
 /*int MyGPIO_Read(GPIO_TypeDef * GPIO, char GPIO_Pin) {
 
 	int value;
 	int pin = (int)GPIO_Pin;
 	
-	// Selon le numéro de pin on regarde le CRL (0 à 7) ou le CRH (8 à 15)
+	// Selon le numÃ©ro de pin on regarde le CRL (0 Ã  7) ou le CRH (8 Ã  15)
 	// https://gist.github.com/iwalpola/6c36c9573fd322a268ce890a118571ca
 	if (pin > 7) {
 		value = GPIO->CRH;
@@ -44,7 +44,7 @@ void MyGPIO_Toggle(GPIO_TypeDef * GPIO, char GPIO_Pin) {
 	
 
 	
-	// On inverse le bit à la position voulue.
+	// On inverse le bit Ã  la position voulue.
 	GPIO->ODR ^= (0x1 << GPIO_Pin);
 }
 
@@ -52,15 +52,15 @@ void MyGPIO_Toggle(GPIO_TypeDef * GPIO, char GPIO_Pin) {
 void MyGPIO_Init (MyGPIO_Struct_TypeDef * GPIOStructPtr){
 	
 	if(GPIOStructPtr->GPIO_Pin < 8){
-		//Mise à 0 des 4 bits correspondant dans CRL
-		GPIOStructPtr->GPIO->CRL &= ~(0xF << (GPIOStructPtr->GPIO_Pin)*4); //si on est au-délà pin 7 -> CRH et et one repart à 0 donc -8
-		//Sélection du mode et de la config de la pin (Sur 4 bits : 0b(2 bits CNF)(2 bits MODE) )
-		GPIOStructPtr->GPIO->CRL |= GPIOStructPtr->GPIO_Conf << (GPIOStructPtr->GPIO_Pin*4);
+		//Mise Ã  0 des 4 bits correspondant dans CRL
+		GPIOStructPtr->GPIO->CRL &= ~(0xF << (GPIOStructPtr->GPIO_Pin)*4); //si on est au-dÃ©lÃ  pin 7 -> CRH et et one repart Ã  0 donc -8
+		//SÃ©lection du mode et de la config de la pin (Sur 4 bits : 0b(2 bits CNF)(2 bits MODE) )
+		GPIOStructPtr->GPIO->CRL |= GPIOStructPtr->GPIO_Conf << (GPIOStructPtr->GPIO_Pin*4 - 8);
 	}else{
-		//Mise à 0 des 4 bits correspondant dans CRH
-		GPIOStructPtr->GPIO->CRH &= ~(0xF << (GPIOStructPtr->GPIO_Pin)*4); //si on est au-délà pin 7 -> CRH et et one repart à 0 donc -8
-		//Sélection du mode et de la config de la pin (Sur 4 bits : 0b(2 bits CNF)(2 bits MODE) )
-		GPIOStructPtr->GPIO->CRH |= GPIOStructPtr->GPIO_Conf << (GPIOStructPtr->GPIO_Pin*4);
+		//Mise Ã  0 des 4 bits correspondant dans CRH
+		GPIOStructPtr->GPIO->CRH &= ~(0xF << (GPIOStructPtr->GPIO_Pin)*4); //si on est au-dÃ©lÃ  pin 7 -> CRH et et one repart Ã  0 donc -8
+		//SÃ©lection du mode et de la config de la pin (Sur 4 bits : 0b(2 bits CNF)(2 bits MODE) )
+		GPIOStructPtr->GPIO->CRH |= GPIOStructPtr->GPIO_Conf << (GPIOStructPtr->GPIO_Pin*4 - 8);
 	}
 
 }
